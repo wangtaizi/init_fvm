14ceaa37897bdffe1ed387e9b94ceca773f7bc1f
amta3208 <77416452+amta3208@users.noreply.github.com>
2021-06-28 11:08:58 -0600

    addition of arithmetic average interpolation

---
+++ interpol/arithmeticAvg.jl
@@ -0,0 +1,33 @@
+function arithmeticAvg(ϕ::CellVariable)
+    #Calculate arithmetic average over mesh
+    #domain of field variable for uniform mesh
+
+    dim = ϕ.domain.dimension
+
+    if dim == 1
+        dx      = ϕ.domain.cellSize.s
+        xval    = (dx[1:end-1].*ϕ.val[1:end-1]+dx[2:end].*ϕ.val[2:end])./
+                    (dx[2:end]+dx[1:end-1])
+        yval    = [ ]
+        zval    = [ ]
+
+    elseif dim == 2
+        nx      = ϕ.domain.dims[1]
+        ny      = ϕ.domain.dims[2]
+        dx      = repeat(ϕ.domain.cellSize.x, (1,ny))
+        dy      = repeat(ϕ.domain.cellSize.y', (nx,1))
+
+        xval    = (dy[1:end-1,:].*ϕ.val[1:end-1,2:end-1]+
+                    dx[2:end,:].*ϕ.val[2:end,2:end-1])./
+                    (dx[2:end,:]+dx[1:end-1,:])
+
+        yval    = (dy[:,1:end-1].*ϕ.val[2:end-1,1:end-1]+
+                    dy[:,2:end].*ϕ.val[2:end-1,2:end])./
+                    (dy[:,2:end]+dy[:,1:end-1])
+
+        zval    = [ ]
+
+    avg = FaceVariable(ϕ.domain, xval, yval, zval)
+
+    return avg
+end
