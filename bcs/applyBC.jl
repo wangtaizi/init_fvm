3564e7da32f882f3593ae31cc2a4a1995bb76f81
amta3208 <77416452+amta3208@users.noreply.github.com>
2021-06-28 08:39:20 -0600

    expansion to 2D

--- bcs/applyBC.jl
+++ bcs/applyBC.jl
@@ -8,8 +8,8 @@ function applyBC(BC::BoundaryCondition)
 
     if dim == 1
         bc_mat, bc_rhs = applyBC_1D(BC)
-    #else dim == 2
-    #    [bc_mat, bc_rhs] = applyBC_2D(BC)
+    else dim == 2
+        [bc_mat, bc_rhs] = applyBC_2D(BC)
     end
 
     return bc_mat, bc_rhs
@@ -25,7 +25,7 @@ function applyBC_1D(BC::BoundaryCondition)
     nx      = BC.domain.dims[1]
     dx_i    = BC.domain.cellSize.x[1]
     dx_f    = BC.domain.cellSize.x[end]
-    domain  = [1:nx+2]
+    domain  = 1:nx+2
     boundaryNodes = 8
 
     #Initialize vectors of indicies
@@ -40,6 +40,7 @@ function applyBC_1D(BC::BoundaryCondition)
 
     #Right Boundary Condition
     i       = nx+2
+    q       = 0
 
     q       += 1
     ii[q]   = domain[i]
@@ -63,13 +64,114 @@ function applyBC_1D(BC::BoundaryCondition)
 
     q       += 1
     ii[q]   = domain[i]
-    jj[q]   = domain[i-1]
+    jj[q]   = domain[i]
     s[q]    = -(BC.right.dir/2 - BC.right.neu/dx_i)
 
     bc_rhs[domain[i]] = -(BC.left.val)
 
     #Formulate Sparse Matrix
-    bc_matrix = sparse(ii[1:q], jj[1:q], s[1:q], [nx+2,nx+2])
+    bc_matrix = sparse(ii[1:q], jj[1:q], s[1:q], nx+2, nx+2)
+
+    return bc_matrix, bc_rhs
+end
+
+function applyBC_2D(BC::BoundaryCondition)
+    #===========================================
+    Generate boundary condition matrix of
+    coefficients and RHS vector for a 1D domain
+    ===========================================#
+
+    #Mesh structure data
+    nx      = BC.domain.dims[1]
+    ny      = BC.domain.dims[2]
+    dx_i    = BC.domain.cellSize.x[1]
+    dx_f    = BC.domain.cellSize.x[end]
+    dy_i    = BC.domain.cellSize.y[1]
+    dy_f    = BC.domain.cellSize.y[end]
+    domain  = 1:nx+2
+    boundaryNodes = 8*(nx+ny+2)
+    nodes   = reshape(1:(nx+2)*(ny+2), (nx+2,ny+2))
+
+    #Initialize vectors of indicies
+    ii  = zeros(boundaryNodes, 1)
+    jj  = zeros(boundaryNodes, 1)
+    s   = zeros(boundaryNodes, 1)
+
+    #Initalize RHS vector
+    bc_rhs = zeros((nx+2)*(ny+2), 1)
+
+    #Generate BC matrix and RHS vector
+
+    #Assign zeros to unused corner nodes
+    q       = 1:4
+    ii[q]   = BC.domain.cornerNodes
+    jj[q]   = BC.domain.cornerNodes
+    s[q]    = max(BC.top.dir/2 + BC.top.neu/dy_f)
+    bc_rhs[BC.domain.cornerNodes] = 0
+
+    #Top Boundary Condition
+    i       = ny+2
+    j       = 2:nx+1
+    q       = q[end]+(1:nx)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i,j]
+    s[q]    = BC.top.dir/2 + BC.top.neu/dy_f
+
+    q       = q[end]+(1:ny)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i,j-1]
+    s[q]    = BC.top.dir/2 - BC.top.neu/dy_f
+
+    bc_rhs[nodes[i,j]] = BC.top.val
+
+    #Bottom Boundary Condition
+    i       = ny+2
+    j       = 2:nx+1
+    q       = q[end]+(1:nx)
+    ii[q]   = nodes[i,j+1]
+    jj[q]   = nodes[i,j]
+    s[q]    = -(BC.bottom.dir/2 + BC.bottom.neu/dy_i)
+
+    q       = q[end]+(1:ny)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i,j]
+    s[q]    = -(BC.bottom.dir/2 - BC.bottom.neu/dy_i)
+
+    bc_rhs[nodes[i,j]] = -(BC.bottom.val)
+
+    #Right Boundary Condition
+    i       = nx+2
+    j       = 2:ny+1
+    q       = q[end]+(1:ny)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i,j]
+    s[q]    = BC.right.dir/2 + BC.right.neu/dx_f
+
+    q       = q[end]+(1:ny)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i-1,j]
+    s[q]    = BC.right.dir/2 - BC.right.neu/dx_f
+
+    bc_rhs[nodes[i,j]] = BC.right.val
+
+    #Left Boundary Condition
+    i       = 1
+    j       = 2:ny+1
+    q       = q[end]+(1:ny)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i+1,j]
+    s[q]    = -(BC.left.dir/2 + BC.left.neu/dx_i)
+
+    q       = q[end]+(1:ny)
+    ii[q]   = nodes[i,j]
+    jj[q]   = nodes[i,j]
+    s[q]    = -(BC.left.dir/2 - BC.left.neu/dx_i)
+
+    bc_rhs[domain[i]] = -(BC.left.val)
+
+    #Formulate Sparse Matrix
+    bc_matrix = sparse(ii[1:q], jj[1:q], s[1:q],
+                    (nx+2)*(ny+2), (nx+2)*(ny+2))
 
     return bc_matrix, bc_rhs
 end
--- discret/diffusionCD.jl
+++ discret/diffusionCD.jl
@@ -1,3 +1,5 @@
+using SparseArrays
+
 function diffusionCD(D::FaceVariable)
     #=======================================
     Given the diffusion coefficient, returns
@@ -7,8 +9,10 @@ function diffusionCD(D::FaceVariable)
 
     dim = D.domain.dimension
 
-    if d == 1
+    if dim == 1
         M = diffusionCD_1D(D)
+    elseif dim == 2
+        M = diffusionCD_2D(D)
     end
 
     return M
@@ -22,10 +26,10 @@ function diffusionCD_1D(D::FaceVariable)
 
     #Mesh data
     nx  = D.domain.dims[1]
-    dom = [1:Nx+2]
+    dom = 1:nx+2
     D_x = D.domain.cellSize.x # x data of mesh
     dx  = 0.5*(D_x[1:end-1] + D_x[2:end])
-    Dx  = D.xval #x component of face value, way too many dx's here :(
+    Dx  = D.x #x component of face value, way too many dx's here :(
 
     #initialize vectors used to define sparse matrix
     ii  = zeros(3*(nx+2), 1)
@@ -38,17 +42,89 @@ function diffusionCD_1D(D::FaceVariable)
     D_r = Dx[2:nx+1]./(dx[2:nx+1].*D_x[2:nx+1])
 
     #Formulate internal cells coefficients
-    val_l  = reshape(D_l, [nx,1])
-    val_r  = reshape(D_r, [nx,1])
+    val_l  = reshape(D_l, (nx,1))
+    val_r  = reshape(D_r, (nx,1))
     val_p = -(val_l + val_r)
 
     #Sparse matrix generation
-    ii[1:3*nx] = repeat(reshape(dom[2:nx+1], [nx, 1]), outer = [3, 1])
-    jj[1:3*nx] = [reshape(dom[1:nx], [nx, 1]); reshape(dom[2:nx+1], [nx, 1])
-                    reshape(dom[3:nx+2], [nx, 1])]
+    ii[1:3*nx] = [reshape(dom[2:nx+1], (nx, 1)); reshape(dom[2:nx+1], (nx, 1));
+                    reshape(dom[2:nx+1], (nx, 1))]
+    #ii[1:3*nx] = repeat(reshape(dom[2:nx+1], (nx, 1)), outer = [3, 1])
+    jj[1:3*nx] = [reshape(dom[1:nx], (nx, 1)); reshape(dom[2:nx+1], (nx, 1))
+                    reshape(dom[3:nx+2], (nx, 1))]
     s[1:3*nx]  = [val_l; val_p; val_r]
 
-    M          = sparse(ii[1:3*nx], jj[1:3*nx], s[1:3*nx], [nx+2, nx+2])
+    M          = sparse(ii[1:3*nx], jj[1:3*nx], s[1:3*nx], nx+2, nx+2)
 
     return M
+end
+
+function diffusionCD_2D(D::FaceVariable)
+    #========================================
+    Returns a discretized diffusion term in 2D
+    using the method of central differences
+    ========================================#
+
+    #Mesh data
+    nx  = D.domain.dims[1]
+    ny  = D.domain.dims[2]
+    nodes   = reshape(1:(nx+2)*(ny+2), (nx+2,ny+2))
+
+    D_x = D.domain.cellSize.x # x data of mesh
+    D_y = D.domain.cellSize.y
+    dx  = 0.5*(D_x[1:end-1,:] + D_x[2:end,:])
+    dy  = 0.5*(D_y[:,1:end-1] + D_y[:,2:end])
+    Dx  = D.x #x component of face value, way too many dx's here :(
+    Dy  = D.y
+
+    #initialize vectors used to define sparse matrix
+    iix  = zeros(3*(nx+2)*(ny+2), 1)
+    jjx  = zeros(3*(nx+2)*(ny+2), 1)
+    sx   = zeros(3*(nx+2)*(ny+2), 1)
+
+    iiy  = zeros(3*(nx+2)*(ny+2), 1)
+    jjy  = zeros(3*(nx+2)*(ny+2), 1)
+    sy   = zeros(3*(nx+2)*(ny+2), 1)
+
+    #Reassign directional velocity vectors
+    # of each direction
+    D_l = Dx[1:nx]./(dx[1:nx].*D_x[2:nx+1]) #left
+    D_r = Dx[2:nx+1]./(dx[2:nx+1].*D_x[2:nx+1]) #right
+    D_t = Dy[:,2:ny+1]./(dy[:,2:ny+1].*D_y[:,2:ny+1]) #top
+    D_b = Dy[:,1:ny]./(dy[:,1:ny].*D_y[:,2:ny+1]) #bottom
+
+    #Formulate internal cells coefficients
+    val_l  = reshape(D_l, (nx*ny,1))
+    val_r  = reshape(D_r, (nx*ny,1))
+    val_t  = reshape(D_t, (nx*ny,1))
+    val_b  = reshape(D_b, (nx*ny,1))
+
+    val_px = reshape(-(val_l + val_r), (nx*ny,1))
+    val_py = reshape(-(val_t + val_b), (nx*ny,1))
+
+    #Sparse matrix generation
+    iix[1:3*(nx*ny)] = [reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1))]
+    jjx[1:3*(nx*ny)] = [reshape(nodes[1:nx, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[3:nx+2, 2:ny+1], (nx*ny, 1));]
+    sx[1:3*(nx*ny)]  = [val_l; val_px; val_r]
+
+
+    iiy[1:3*(nx*ny)] = [reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1))]
+    jjy[1:3*(nx*ny)] = [reshape(nodes[2:nx+1, 1:ny], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 2:ny+1], (nx*ny, 1));
+                            reshape(nodes[2:nx+1, 3:ny+2], (nx*ny, 1));]
+    sy[1:3*(nx*ny)]  = [val_b; val_py; val_t]
+
+    Mx              = sparse(iix[1:3*(nx*ny)], jjx[1:3*(nx*ny)], sx[1:3*(nx*ny)],
+                    (nx+2)*(ny+2), (nx+2)*(ny+2))
+    My              = sparse(iiy[1:3*(nx*ny)], jjy[1:3*(nx*ny)], sy[1:3*(nx*ny)],
+                                    (nx+2)*(ny+2), (nx+2)*(ny+2))
+    M               = Mx + My
+
+    return M                                 
 end
