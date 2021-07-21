4eabb2bfaf894b67f9796427fd9d3763cc51ac56
amta3208 <77416452+amta3208@users.noreply.github.com>
2021-07-21 13:28:27 -0600

    corrected handling of ghost cells and handling of cell volumes

--- navier_stokes/RhieChow.jl
+++ navier_stokes/RhieChow.jl
@@ -1,34 +1,52 @@
-function RhieChow(u::CellVariable, v::CellVariable, uap::CellVariable,
-                    vap::CellVariable, p::CellVariable)
+function RhieChow(u::CellVariable, v::CellVariable, u_ap::CellVariable,
+                    v_ap::CellVariable, p::CellVariable)
     #Returns face values of given field variable
     #within a 2D cartestian grid using the collated
     #Rhie-Chow interpolation method
 
     #Interpolated face average of the velocity components
     uFace       = arithmeticFaceAvg(u)
+    #print(uFace.x[1:5])
     vFace       = arithmeticFaceAvg(v)
 
     #Interpolated face average of the ap coefficients of
     #the velocity values
-    uapFace     = arithmeticFaceAvg(uap)
-    vapFace     = arithmeticFaceAvg(vap)
+    u_apFace     = arithmeticFaceAvg(u_ap)
+    v_apFace     = arithmeticFaceAvg(v_ap)
 
     #cell and face gradients of pressure
-    presFaceGrad            = faceGrad(p)
-    presGradx, presGrady    = grad(p)
+    presFaceGrad                = faceGrad(p)
+    presFaceGrad.x[:,[1 end]]  .= 0
+    presFaceGrad.y[[1 end],:]  .= 0
+    #println(presFaceGrad.x)
+
+    presGradx, presGrady        = grad(p)
+    #println(presGradx.val)
 
     #interpolated face average of the cell pressure gradient
-    presGradxFace   = arithmeticFaceAvg(presGradx)
-    presGradyFace   = arithmeticFaceAvg(presGrady)
+    presGradxFace                = arithmeticFaceAvg(presGradx)
+    presGradxFace.x[:,[1 end]]  .= 0
+
+    presGradyFace                = arithmeticFaceAvg(presGrady)
+    presGradyFace.y[[1 end], :] .= 0
+
+    #Calculate cell volumes and take arithmetic average
+    vol     = generateCellVar(p.domain,
+                    p.domain.cellSize.x * p.domain.cellSize.y')
+    volFace = arithmeticFaceAvg(vol)
 
     #Define and update starred face velocities
-    faceVel    = FaceVariable(u.domain, xval, yval) #xval, yval not defined
+    faceVelx        = uFace.x + volFace.x ./ u_apFace.x .*
+                    (presGradxFace.x - presFaceGrad.x)
+    faceVelx[1,:]  .= 0
+    faceVelx[end,:].= 0
 
-    faceVel.x  = uFace.x + (p.domain.cellSize.x .* p.domain.cellSize.y) ./
-                    uapFace.x .* (presGradxFace - presFaceGrad.x)
+    faceVely        = vFace.y + volFace.y ./ v_apFace.y .*
+                    (presGradyFace.y - presFaceGrad.y)
+    faceVely[:,1]  .= 0
+    faceVely[:,end].= 0
 
-    faceVel.y  = vFace.y + (p.domain.cellSize.x .* p.domain.cellSize.y) ./
-                    vapFace.y .* (presGradyFace - presFaceGrad.y)
+    faceVel    = FaceVariable(u.domain, faceVelx, faceVely, [])
 
 
     return faceVel
